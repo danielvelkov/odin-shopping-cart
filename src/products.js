6f7d0c401e8d192e19b0bc1855e741f3bcb910db
Daniel Velkov <danielvelkov13@gmail.com>
2025-02-08 15:03:19 +0200

    feat: add methods for fetching products
    Add a file which includes methods for retrieving
    products from the MOCK API - 'Fake Storage API'.
    
    Add dependency 'match-sorter' to filter the
    products based on their title and description if
    a query is provided.

---
+++ src/products.js
@@ -0,0 +1,31 @@
+import { matchSorter } from "match-sorter";
+
+export async function getProducts(query) {
+  let response = await fetch("https://fakestoreapi.com/products");
+  if (!response.ok)
+    throw new Response("Server error", {
+      status: response.status,
+      statusText: response.statusText,
+    });
+  let products = await response.json();
+  if (!products) products = [];
+
+  if (query)
+    products = matchSorter(products, query, { keys: ["title", "description"] });
+
+  return products;
+}
+
+export async function getProduct(id) {
+  let response = await fetch("https://fakestoreapi.com/products/" + id);
+  if (!response.ok)
+    throw new Response("Server error", {
+      status: response.status,
+      statusText: response.statusText,
+    });
+
+  if (!response.data) return {};
+
+  let product = await response.json();
+  return product;
+}
