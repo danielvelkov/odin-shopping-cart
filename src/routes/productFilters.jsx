da50436640b1d0534770ae0f36a4f664ef7a56e5
Daniel Velkov <danielvelkov13@gmail.com>
2025-03-01 18:16:56 +0200

    feat: integrate the starRatingFilter into productFilters
    Now the starRatingFilter appears alongside the other filters.
    
    Add state for minimum rating selected.
    
    Updating the value of the minimum rating is debounced.
    
    Filter is active if toggled or if 'minPrice' url search param is present.
    
    Updating the filter value updates the URLsearchparams.

--- src/routes/productFilters.jsx
+++ src/routes/productFilters.jsx
@@ -2,17 +2,21 @@ import { useSearchParams } from "react-router-dom";
 import PriceRangeSlider from "../components/priceRangeSlider";
 import useDebounce from "../hooks/useDebounce";
 import { useEffect, useState } from "react";
+import StarRatingFilter from "../components/starRatingFilter";
 
 const MIN_PRICE_LIMIT = 0;
 const MAX_PRICE_LIMIT = 10000;
+const DEFAULT_MIN_RATING = 0;
 
 const ProductFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const isPriceSliderActive =
     searchParams.has("minPrice") || searchParams.has("maxPrice");
+  const isRatingFilterActive = searchParams.has("minRating");
 
   const [minPrice, setMinPrice] = useState(MIN_PRICE_LIMIT);
   const [maxPrice, setMaxPrice] = useState(MAX_PRICE_LIMIT);
+  const [minStarRating, setMinStarRating] = useState(DEFAULT_MIN_RATING);
 
   const updatePriceRangeParams = useDebounce(({ min, max }) => {
     if (isPriceSliderActive)
@@ -24,9 +28,21 @@ const ProductFilters = () => {
       });
   });
 
+  const updateRatingParams = useDebounce(({ minRating }) => {
+    if (isRatingFilterActive)
+      setSearchParams((prev) => {
+        const newParams = new URLSearchParams(prev);
+        newParams.set("minRating", minRating);
+        return newParams;
+      });
+  });
+
   useEffect(() => {
     setMinPrice(Number(searchParams.get("minPrice") ?? MIN_PRICE_LIMIT));
     setMaxPrice(Number(searchParams.get("maxPrice") ?? MAX_PRICE_LIMIT));
+    setMinStarRating(
+      Number(searchParams.get("minRating") ?? DEFAULT_MIN_RATING),
+    );
   }, [searchParams]);
 
   const handlePriceChange = ({ min, max }) => {
@@ -35,15 +51,32 @@ const ProductFilters = () => {
     updatePriceRangeParams({ min, max });
   };
 
+  const handleRatingChange = ({ minRating }) => {
+    setMinStarRating(minRating);
+    updateRatingParams({ minRating });
+  };
+
   const handlePriceRangeToggle = (e) => {
     setSearchParams((prev) => {
       const newParams = new URLSearchParams(prev);
-      if (e.target.checked && e.isTrusted) {
+      if (e.target.checked) {
         newParams.set("minPrice", minPrice);
         newParams.set("maxPrice", maxPrice);
       } else {
         newParams.delete("minPrice");
         newParams.delete("maxPrice");
+      }
+      return newParams;
+    });
+  };
+
+  const handleStarRatingToggle = (e) => {
+    setSearchParams((prev) => {
+      const newParams = new URLSearchParams(prev);
+      if (e.target.checked) {
+        newParams.set("minRating", minStarRating);
+      } else {
+        newParams.delete("minRating");
       }
       return newParams;
     });
@@ -60,6 +93,13 @@ const ProductFilters = () => {
         handleToggle={handlePriceRangeToggle}
         handleChange={handlePriceChange}
       />
+      <StarRatingFilter
+        selectedRating={minStarRating}
+        minStars={1}
+        maxStars={5}
+        handleToggle={handleStarRatingToggle}
+        handleChange={handleRatingChange}
+      ></StarRatingFilter>
     </>
   );
 };
