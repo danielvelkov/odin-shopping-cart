d9edba63c979becc9345cf471724cce13d4019c8
Daniel Velkov <danielvelkov13@gmail.com>
2025-03-03 18:56:02 +0200

    feat: add sorting options to the filter bar in 'products' route
    Add sorting options names as separate js file in order
    to keep track of them easier and not make spelling errors.
    
    Add module 'stringCaseConverter' that will contain
    methods that manipulate the string case.
    Has method that converts camelCase to Title Case.
    
    'products' route has another form in the filterbar
    containing various sort options.
    Only 1 type of sorting is active at a time.
    Selecting the button for that sorting again disables it.

---
+++ src/constants/productSortingNames.js
@@ -0,0 +1,6 @@
+export const ProductSortingNames = {
+  BY_PRICE: "priceDescending",
+  BY_PRICE_ASCENDING: "priceAscending",
+  BY_RATING: "ratingDescending",
+  BY_RATING_ASCENDING: "ratingAscending",
+};
--- src/routes/products.jsx
+++ src/routes/products.jsx
@@ -1,8 +1,10 @@
 import {
   Form,
   useLoaderData,
+  useLocation,
   useNavigate,
   useNavigation,
+  useSearchParams,
 } from "react-router-dom";
 import { getProducts } from "/src/products";
 import ProductCard from "/src/components/productCard";
@@ -12,6 +14,9 @@ import { useEffect } from "react";
 import { getProductsByCategory } from "../products";
 import LoadingSpinner from "../components/loadingSpinner";
 import { FilterNames } from "../constants/filterNames";
+import CheckmarkButton from "../components/checkmarkButton";
+import { ProductSortingNames } from "../constants/productSortingNames";
+import { camelCaseToWords } from "../utils/stringCaseConverter";
 
 function filterProductsBySearchParams(products, searchParams) {
   const minProductPrice = searchParams.get(FilterNames.MIN_PRICE);
@@ -26,39 +31,82 @@ function filterProductsBySearchParams(products, searchParams) {
   return products;
 }
 
+function sortProducts(products, sortingType) {
+  switch (sortingType) {
+    case ProductSortingNames.BY_PRICE_ASCENDING:
+      products.sort((p1, p2) => p1.price - p2.price);
+      break;
+    case ProductSortingNames.BY_PRICE:
+      products.sort((p1, p2) => p2.price - p1.price);
+      break;
+    case ProductSortingNames.BY_RATING_ASCENDING:
+      products.sort((p1, p2) => p1.rating.rate - p2.rating.rate);
+      break;
+    case ProductSortingNames.BY_RATING:
+      products.sort((p1, p2) => p2.rating.rate - p1.rating.rate);
+      break;
+    default:
+  }
+  return products;
+}
+
 export async function loader({ request }) {
   const url = new URL(request.url);
   const searchQuery = url.searchParams.get("q");
+  const sortingType = url.searchParams.get("sort");
 
   let products = await getProducts(searchQuery);
   products = filterProductsBySearchParams(products, url.searchParams);
-  return { products, query: searchQuery };
+  products = sortProducts(products, sortingType);
+  return { products, query: searchQuery, sorting: sortingType };
 }
 
 export async function categoryProductsLoader({ params, request }) {
   const url = new URL(request.url);
   const searchQuery = url.searchParams.get("q");
+  const sortingType = url.searchParams.get("sort");
 
   let products = await getProductsByCategory(params.category, searchQuery);
   products = filterProductsBySearchParams(products, url.searchParams);
-  return { products, query: searchQuery };
+  products = sortProducts(products, sortingType);
+  return { products, query: searchQuery, sorting: sortingType };
 }
 
 const Products = () => {
   const navigate = useNavigate();
   const navigation = useNavigation();
+  const location = useLocation();
+  const { products, query, sorting } = useLoaderData();
+  const [searchParams] = useSearchParams();
+
   const isNavigating = Boolean(navigation.location);
-  const { products, query } = useLoaderData();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const currentParams = Object.fromEntries(searchParams.entries());
+    const formData = new FormData(event.target);
+    const formParams = Object.fromEntries(formData.entries());
+    const mergedParams = { ...currentParams, ...formParams };
+    const queryString = new URLSearchParams(mergedParams).toString();
+    // Navigate to the new URL
+    navigate(`${location.pathname}?${queryString}`);
+  };
 
   useEffect(() => {
     document.getElementById("q").value = query ?? "";
   }, [query]);
 
+  const handleSortingButtonClick = (sortingType) => {
+    if (sorting !== sortingType)
+      document.getElementById("sort-param").value = sortingType;
+    else document.getElementById("sort-param").name = ""; //removes it
+  };
+
   return (
     <>
       <h2>Our Products</h2>
       <FilterBar>
-        <Form id="product-search">
+        <Form id="product-search" onSubmit={handleSubmit}>
           <input
             id="q"
             aria-label="Search products"
@@ -78,7 +126,21 @@ const Products = () => {
               : "no results found"}
           </span>
         </Form>
+
+        <Form id="product-sort" onSubmit={handleSubmit}>
+          <input type="hidden" id="sort-param" name="sort"></input>
+          {Object.values(ProductSortingNames).map((value) => (
+            <CheckmarkButton
+              key={value}
+              active={sorting === value}
+              handleClick={() => handleSortingButtonClick(value)}
+            >
+              {camelCaseToWords(value)}
+            </CheckmarkButton>
+          ))}
+        </Form>
       </FilterBar>
+
       {isNavigating ? (
         <LoadingSection>
           <LoadingSpinner></LoadingSpinner>
@@ -140,6 +202,7 @@ const CardList = styled.ul`
 const FilterBar = styled.div`
   display: flex;
   align-items: baseline;
+  justify-content: space-between;
   flex-wrap: wrap;
   gap: 0.5em;
 
@@ -151,6 +214,10 @@ const FilterBar = styled.div`
     &:hover {
       box-shadow: 0 0 10px rgb(0, 0, 0, 0.3);
     }
+  }
+  #product-sort {
+    display: flex;
+    gap: 1em;
   }
 
   form button[type="submit"] {
